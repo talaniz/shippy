00f97823617a8408d08a7bbc9034dad3fa765819
Antonio Alaniz <antonio.alaniz@gmail.com>
2021-10-20 19:36:16 -0700

    Dockerized grpc server

--- shippy-service-consignment/go.mod
+++ shippy-service-consignment/go.mod
@@ -3,9 +3,14 @@ module github.com/talaniz/shippy/shippy-service-consignment
 go 1.15
 
 require (
+	github.com/golang/protobuf v1.5.2
+	github.com/micro/go-micro/v2 v2.9.1 // indirect
+	github.com/micro/micro/v2 v2.9.2-0.20200728090142-c7f7e4a71077 // indirect
+	github.com/spf13/viper v1.6.3 // indirect
 	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
 	golang.org/x/sys v0.0.0-20211015200801-69063c4bb744 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/genproto v0.0.0-20211018162055-cf77aa76bad2 // indirect
 	google.golang.org/grpc v1.41.0
+	google.golang.org/protobuf v1.27.1
 )
--- shippy-service-consignment/main.go
+++ shippy-service-consignment/main.go
@@ -18,6 +18,7 @@ const (
 
 type repository interface {
 	Create(*pb.Consignment) (*pb.Consignment, error)
+	GetAll() []*pb.Consignment
 }
 
 type Repository struct {
@@ -33,6 +34,10 @@ func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, er
 	return consignment, nil
 }
 
+func (repo *Repository) GetAll() []*pb.Consignment {
+	return repo.consignments
+}
+
 type service struct {
 	repo repository
 }
@@ -45,6 +50,11 @@ func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*
 	}
 
 	return &pb.Response{Created: true, Consignment: consignment}, nil
+}
+
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest) (*pb.Response, error) {
+	consignments := s.repo.GetAll()
+	return &pb.Response{Consignments: consignments}, nil
 }
 
 func main() {
